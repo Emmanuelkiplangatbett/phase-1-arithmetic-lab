70e2115ea71949a491ae8260b7c670db26300ef7
emmanuelkiplangatbett <emmanuel.bett@student.moringa>
2022-11-24 06:34:24 +0300

    completed assignments

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
-// Write your code here
+// Write your code here let num1 =31;
+   
+    let num1 =2;
+    let num2 =31;
+    let multiply=num1 * num2;
+   
+
+  //random integer
+  let random =Math.floor(Math.random()*10)
+  console.log(random)
+ //remainder
+ let num3 =10;
+ let num4 =6;
+ let mod =num3 % num4
+ //max integer
+ let max =Math.max(20,0);
+   
